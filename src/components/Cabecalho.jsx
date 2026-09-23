c9bb254577a7e56b8168d763a95c7b8bd90afcd1
Enzo Belizardo <enzo.s.belizardo@outlook.com>
2026-09-23 09:43:13 -0300

    Tentativa upload

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,13 @@
+function Cabecalho() {
+    return (
+        <header className="cabecalho">
+            <div className="cabecalho-conteudo">
+                <h1>Sistema de Gestão de Chamados</h1>
+                <p>Gerenciamento de Chamados</p>
+                <span style={{fontWeight: "bold", color: "black"}}>Desafio Tecnico. Desenvolvindo por Henrique Porto de Sousa</span>
+            </div>
+        </header>
+    )
+}
+
+export default Cabecalho
