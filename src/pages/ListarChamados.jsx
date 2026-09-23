521b5f650b7f0f88e154d0c469adfdb563572a3a
Enzo Belizardo <enzo.s.belizardo@outlook.com>
2026-09-23 10:11:41 -0300

    Correção
    Updated author name in the ListarChamado component.

--- src/pages/ListarChamados.jsx
+++ src/pages/ListarChamados.jsx
@@ -15,7 +15,7 @@ function ListarChamado({ chamados, aoExcluir }) {
     return (
         <main className="pagina">
             <h1>Lista de chamados</h1>
-            <span style={{ fontWeight: "bold", color: "black" }}>Desafio Tecnico. Desenvolvindo por Henrique Porto de Sousa</span>
+            <span style={{ fontWeight: "bold", color: "black" }}>Feito por: Enzo Silva Belizardo </span>
             <ul className="lista">
                 {chamados.map((chamado) => (
                     <li key={chamado.id}>
