8bfb94922d6f1918cb745c0efd7ef4c5e5f6d366
Enzo Belizardo <enzo.s.belizardo@outlook.com>
2026-09-23 09:59:36 -0300

    Cabecalho obrigatorio
    Updated the author name in the header component.

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -4,7 +4,7 @@ function Cabecalho() {
             <div className="cabecalho-conteudo">
                 <h1>Sistema de Gestão de Chamados</h1>
                 <p>Gerenciamento de Chamados</p>
-                <span style={{fontWeight: "bold", color: "black"}}>Desafio Tecnico. Desenvolvindo por Henrique Porto de Sousa</span>
+                <span style={{fontWeight: "bold", color: "black"}}>Desafio feito por: Enzo Silva Belizardo</span>
             </div>
         </header>
     )
